6124e280d0372d08663ad6ecfde28da03f20209b
liangsingle <112257818+liangsingle@users.noreply.github.com>
2026-04-05 16:43:49 +0800

    Update copyright.vue

--- src/components/copyright.vue
+++ src/components/copyright.vue
@@ -19,7 +19,7 @@ import * as pkg from "../../package.json";
       <a class="copyright-p-1-1" href="https://sponsors.yunyoujun.cn" target="_blank" rel="noreferrer">
         <div class="copyright-p-1-1-icon" />
       </a>
-      <a :href="pkg.author.url" target="_blank" rel="noreferrer">安知鱼 @{{ pkg.author.name }}</a>
+      <a :href="pkg.author.url" target="_blank" rel="noreferrer">小橘子Single @{{ pkg.author.name }}</a>
     </div>
     <p>2022 - {{ new Date().getFullYear() }}</p>
   </div>
